4e3a2b195af321b511ffc661301ff32bbd73a7ab
jeremybroutin <jeremy.broutin@gmail.com>
2015-06-12 16:11:34 +0200

    Added parsing and handling JSON data to display image and title or no content found info

--- FlickFinder/ViewController.swift
+++ FlickFinder/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
   @IBOutlet weak var titleOne: UILabel!
   @IBOutlet weak var titleTwo: UILabel!
+  @IBOutlet weak var defaultTitle: UILabel!
   @IBOutlet weak var imagePlaceholder: UIImageView!
   @IBOutlet weak var titlePlaceholder: UILabel!
   @IBOutlet weak var searchTextField: UITextField!
@@ -25,7 +26,7 @@ class ViewController: UIViewController {
   let METHOD_NAME = "flickr.photos.search"
   let API_KEY = "aed5bae79e7824cc4a13e45c49163a88"
   let EXTRAS = "url_m"
-  let TEXT = "baby+asian+elephant"
+  let TEXT = "screugnen+asian+elephant"
   let DATA_FORMAT = "json"
   let NO_JSON_CALLBACK = "1"
   
@@ -62,8 +63,59 @@ class ViewController: UIViewController {
         println(request)
         //parse the received data
         var parsingError: NSError? = nil
-        let parsedResults: AnyObject! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError)
-        println(parsedResults)
+        let parsedResults: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError)
+        
+        //set json data into a dictionary
+        if let photosDictionary = parsedResults?.valueForKey("photos") as? NSDictionary{
+          
+          var totalPhotosVal = 0
+          if let totalPhotos = photosDictionary["total"] as? String {
+            totalPhotosVal = (totalPhotos as NSString).integerValue
+          }
+          
+          if totalPhotosVal > 0 {
+            //set dictionary content into an array
+            if let photoArray = photosDictionary.valueForKey("photo") as? [[String: AnyObject]] {
+              
+              //grab a single, random image
+              let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+              let photoDictionary = photoArray[randomPhotoIndex] as [String: AnyObject]
+              
+              //get the image url and thte titla
+              let imageUrlString = photoDictionary["url_m"] as? String
+              let imageURL = NSURL(string: imageUrlString!)
+              let photoTitle = photoDictionary["title"] as? String
+              
+              //debug
+              println(imageURL)
+              
+              //check if image exists at the url
+              if let imageData = NSData(contentsOfURL: imageURL!){
+                dispatch_async(dispatch_get_main_queue(), {
+                  self.defaultTitle.hidden = true
+                  self.imagePlaceholder.image = UIImage(data: imageData)
+                  self.titlePlaceholder.text = photoTitle
+                })
+              }
+              else{
+                println("Image doesn't exist at \(imageURL)")
+                dispatch_async(dispatch_get_main_queue(), {
+                  
+                })
+              }
+            }
+          }
+          else{
+            println("There is no photo available.")
+            dispatch_async(dispatch_get_main_queue(), {
+              self.imagePlaceholder.image = UIImage(named: "no image available")
+              self.defaultTitle.enabled = false
+              self.titlePlaceholder.text = "Oups, nothing found... Try another search"
+              self.titlePlaceholder.textAlignment = NSTextAlignment.Center
+            })
+          }
+        }
+      
       }
     }
     task.resume()
